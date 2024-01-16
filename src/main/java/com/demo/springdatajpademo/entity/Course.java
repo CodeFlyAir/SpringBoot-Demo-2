08620e3b37570d0838ed64cbe818123d1359f9e9
Rungshit Saha <95049059+CodeFlyAir@users.noreply.github.com>
2024-01-17 02:57:22 +0530

    Added Course and Course Material. Implemented One-To-One relationship between them

---
+++ src/main/java/com/demo/springdatajpademo/entity/Course.java
@@ -0,0 +1,33 @@
+package com.demo.springdatajpademo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Course {
+    @Id
+    @SequenceGenerator(
+            name = "course_id",
+            sequenceName = "course_id",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_id"
+    )
+    private Long courseId;
+    private String title;
+    private Integer credits;
+
+    @OneToOne(
+            mappedBy = "course"
+    )
+    private CourseMaterial courseMaterial;
+}
